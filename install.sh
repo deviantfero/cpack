001dbdc8ae06b4335867a41207bd70236ee25cd8
deviantfero <fmorataya.04@gmail.com>
2016-04-29 22:02:55 -0600

    changes to aftership html were made, cpack has been adjusted

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp -r ./cpacksrc /usr/local/bin/ && sudo cp ./cpack /usr/local/bin/
-sudo chmod +x /usr/local/bin/cpack
+sudo cp -r ./cpacksrc /usr/local/bin/ && sudo cp ./cpack /usr/local/bin/cpack1
+sudo chmod +x /usr/local/bin/cpack1
