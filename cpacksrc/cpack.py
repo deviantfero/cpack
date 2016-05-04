34dd9e1b2d09de1af1063267f314cbd775cc4201
Fernando Vásquez <fmorataya.04@gmail.com>
2016-05-04 15:56:39 -0500

    updated version

--- cpacksrc/cpack.py
+++ cpacksrc/cpack.py
@@ -75,7 +75,7 @@ class mainWindow( Gtk.Window ):
         self.tracking_active = ""
         self.info_list = []
 
-        Gtk.Window.__init__( self, title="cpack v1.2" )
+        Gtk.Window.__init__( self, title="cpack v1.3" )
         self.set_border_width( 10 )
         self.set_default_size( 600, 100 )
 
